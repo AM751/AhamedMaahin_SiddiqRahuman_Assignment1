50bdd7345c4a8c96760af42eaf55eb2c7dd75e6e
Ahamed Maahin Siddiq Rahuman <ahamaasidrah@gmail.com>
2025-05-08 20:45:25 -0600

    Scene transitions:
    Added code for buttons to act accordingly for the scene transitions in Main menu scene.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void gameStart()
     {
-        
+        SceneManager.LoadScene("Level 1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void quitGame()
     {
-        
+        Application.Quit();
     }
 }
