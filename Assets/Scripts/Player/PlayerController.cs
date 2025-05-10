61e4823098d1c9b201ca5aa515d10e352fa8f03f
Ahamed Maahin Siddiq Rahuman <ahamaasidrah@gmail.com>
2025-05-10 12:59:37 -0600

    Player Controller:
    Added the player controller for the player character.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController playerController;
+    private Vector3 playerVelocity;
+    private bool playerGrounded;
+    private float playerSpeed = 10f;
+    //private float playerJumpHeight = 5f;
+
+
+
+    private void Start()
+    {
+        //To assign the playerController as the CharcaterController:
+
+        playerController = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    //To check whether the player is grounded?
+    {
+        playerGrounded = playerController.isGrounded;
+        if (playerGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        //For the Movement of the player deirections:
+        Vector3 movementSides = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        playerController.Move(movementSides * playerSpeed * Time.deltaTime);
+
+    }
+}
