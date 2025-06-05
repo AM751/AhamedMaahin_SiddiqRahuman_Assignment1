2c15b1eb7ec977f383d804e51c27e99631ce2b62
Ahamed Maahin Siddiq Rahuman <ahamaasidrah@gmail.com>
2025-06-05 17:04:24 -0600

    Bug resolved:
    Resolved the game over canvas bug. Now if the player looses all his/her health, the game over canvas will be appearing.

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -1,11 +1,16 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private HealthController healthBarUI;
     [SerializeField] private Canvas gameOverCanvas;
 
-
+    void Start ()
+    {
+        gameOverCanvas.enabled = false;
+    }
     void OnEnable()
     {
         healthBarUI.OnHealthUpdated += GameOverCanvas;
@@ -16,6 +21,10 @@ public class GameOverUI : MonoBehaviour
         healthBarUI.OnHealthUpdated -= GameOverCanvas;
     }
 
+    public void PlayAgain ()
+    {
+        SceneManager.LoadScene(1);
+    }
     public void GameOverCanvas(float damage, float fullHealth, float currentHealth)
     {
         if (healthBarUI != null && currentHealth <= 0)
